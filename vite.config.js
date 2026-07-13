87f10264401e5d7b537eb0a8401a0daea524622c
Lanowski <96080779+Lanowski@users.noreply.github.com>
2026-07-13 16:13:20 +0200

    Configure Vite for React with custom base path

--- vite.config.js
+++ vite.config.js
@@ -9,3 +9,10 @@ export default defineConfig({
     open: true
   }
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Genealogy/', //
+})
